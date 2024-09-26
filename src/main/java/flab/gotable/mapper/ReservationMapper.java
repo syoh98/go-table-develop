b015392b6f257afbcff12579246c1c0244a98e87
syoh98 <qorxla1004@gmail.com>
2024-09-27 01:09:57 +0900

    네임드락 적용

--- src/main/java/flab/gotable/mapper/ReservationMapper.java
+++ src/main/java/flab/gotable/mapper/ReservationMapper.java
@@ -12,6 +12,10 @@ import java.time.LocalTime;
 public interface ReservationMapper {
     public int getRestaurantLock(long restaurantId);
 
+    public Integer getNamedLock(@Param("lockName") String lockName);
+
+    public Integer releaseNamedLock(@Param("lockName") String lockName);
+
     public boolean isExistDailySchedule(@Param("restaurantId") long restaurantId,
                                         @Param("dayOfWeek") String dayOfWeek,
                                         @Param("startTime") LocalTime startTime,
