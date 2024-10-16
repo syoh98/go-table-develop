075bf815a6fcaf9c9666367a081338a1a4249afa
syoh98 <qorxla1004@gmail.com>
2024-10-16 23:02:50 +0900

    날짜 데이터를 동적으로 가져오도록 수정

--- src/test/java/flab/gotable/service/ReservationServiceTest.java
+++ src/test/java/flab/gotable/service/ReservationServiceTest.java
@@ -1,11 +1,11 @@
 package flab.gotable.service;
 
+import flab.gotable.domain.entity.Reservation;
 import flab.gotable.dto.request.ReservationRequestDto;
 import flab.gotable.dto.response.ReservationResponseDto;
 import flab.gotable.exception.*;
-import flab.gotable.mapper.MemberMapper;
 import flab.gotable.mapper.ReservationMapper;
-import flab.gotable.mapper.StoreMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
@@ -21,6 +21,7 @@ import java.time.LocalDateTime;
 @SpringBootTest
 @Testcontainers
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Slf4j
 class ReservationServiceTest {
 
     @Container
@@ -44,128 +45,118 @@ class ReservationServiceTest {
     private ReservationMapper reservationMapper;
 
     @Autowired
-    private MemberMapper memberMapper;
-
-    @Autowired
-    private StoreMapper storeMapper;
-
-    @Autowired
     private ReservationService reservationService;
+
+    private final LocalDateTime startTime = LocalDateTime.now().plusDays(7).withHour(10).withMinute(0).withSecond(0).withNano(0);
+    private final LocalDateTime endTime = startTime.plusHours(1);
 
     @Test
     @DisplayName("존재하지 않는 회원 seq로 예약하는 경우 MemberNotFoundException 예외를 발생시킨다.")
     void reserveNotExistMember() {
         // given
-        ReservationRequestDto requestDto = new ReservationRequestDto(1L, 99L, LocalDateTime.of(2024, 8, 14, 10, 0), LocalDateTime.of(2023, 8, 14, 11, 0), 3L);
-
-        reservationService = new ReservationService(reservationMapper, memberMapper, storeMapper);
+        ReservationRequestDto requestDto = new ReservationRequestDto(1L, 99L, startTime, endTime, 3L);
 
         // when, then
-        Assertions.assertThrows(MemberNotFoundException.class, () -> { reservationService.reserve(requestDto); });
+        Assertions.assertThrows(MemberNotFoundException.class, () -> { reservationService.reserveNamedLock(requestDto); });
     }
 
     @Test
     @DisplayName("존재하지 않는 식당 id로 예약하는 경우 StoreNotFoundException 예외를 발생시킨다.")
     void reserveNotExistRestaurant() {
         // given
-        ReservationRequestDto requestDto = new ReservationRequestDto(351L, 1L, LocalDateTime.of(2024, 8, 14, 10, 0), LocalDateTime.of(2024, 8, 14, 11, 0), 3L);
-
-        reservationService = new ReservationService(reservationMapper, memberMapper, storeMapper);
+        ReservationRequestDto requestDto = new ReservationRequestDto(351L, 1L, startTime, endTime, 3L);
 
         // when, then
-        Assertions.assertThrows(StoreNotFoundException.class, () -> { reservationService.reserve(requestDto); });
+        Assertions.assertThrows(StoreNotFoundException.class, () -> { reservationService.reserveNamedLock(requestDto); });
     }
 
     @Test
     @DisplayName("0 이하의 인원 수로 예약하는 경우 InvalidReservationMemberCountException 예외를 발생시킨다.")
     void reserveUnderMinMemberCount() {
         // given
-        ReservationRequestDto requestDto = new ReservationRequestDto(1L, 1L, LocalDateTime.of(2024, 8, 14, 10, 0), LocalDateTime.of(2024, 8, 14, 11, 0), 0L);
-
-        reservationService = new ReservationService(reservationMapper, memberMapper, storeMapper);
+        ReservationRequestDto requestDto = new ReservationRequestDto(1L, 1L, startTime, endTime, 0L);
 
         // when, then
-        Assertions.assertThrows(InvalidReservationMemberCountException.class, () -> { reservationService.reserve(requestDto); });
+        Assertions.assertThrows(InvalidReservationMemberCountException.class, () -> { reservationService.reserveNamedLock(requestDto); });
     }
 
     @Test
     @DisplayName("예약 인원 수가 예약 가능 최대 인원 수를 초과했을 경우 InvalidReservationMemberCountException 예외를 발생시킨다.")
     void reserveExceedsMaxMemberCount() {
         // given
-        ReservationRequestDto requestDto = new ReservationRequestDto(1L, 1L, LocalDateTime.of(2024, 8, 14, 10, 0), LocalDateTime.of(2024, 8, 14, 11, 0), 99L);
-
-        reservationService = new ReservationService(reservationMapper, memberMapper, storeMapper);
+        ReservationRequestDto requestDto = new ReservationRequestDto(1L, 1L, startTime, endTime, 99L);
 
         // when, then
-        Assertions.assertThrows(InvalidReservationMemberCountException.class, () -> { reservationService.reserve(requestDto); });
+        Assertions.assertThrows(InvalidReservationMemberCountException.class, () -> { reservationService.reserveNamedLock(requestDto); });
     }
 
     @Test
     @DisplayName("예약 종료 시간이 예약 시작 시간보다 앞서거나 동일한 경우 InvalidReservationTimeException 예외를 발생시킨다.")
     void reserveEndTimeBeforeOrEqualStartTime() {
         // given
-        ReservationRequestDto requestDto = new ReservationRequestDto(1L, 1L, LocalDateTime.of(2024, 8, 14, 10, 0), LocalDateTime.of(2024, 8, 14, 9, 0), 3L);
-
-        reservationService = new ReservationService(reservationMapper, memberMapper, storeMapper);
+        ReservationRequestDto requestDto = new ReservationRequestDto(1L, 1L, endTime, startTime, 3L);
 
         // when, then
-        Assertions.assertThrows(InvalidReservationTimeException.class, () -> { reservationService.reserve(requestDto); });
+        Assertions.assertThrows(InvalidReservationTimeException.class, () -> { reservationService.reserveNamedLock(requestDto); });
     }
 
     @Test
     @DisplayName("예약 시작 시간이 현재 시간보다 이전인 경우 InvalidReservationTimeException 예외를 발생시킨다.")
     void reservePastStartTime() {
         // given
-        ReservationRequestDto requestDto = new ReservationRequestDto(1L, 1L, LocalDateTime.of(2024, 1, 1, 10, 0), LocalDateTime.of(2024, 8, 14, 9, 0), 3L);
-
-        reservationService = new ReservationService(reservationMapper, memberMapper, storeMapper);
+        ReservationRequestDto requestDto = new ReservationRequestDto(1L, 1L, startTime.minusDays(7), endTime.minusDays(7), 3L);
 
         // when, then
-        Assertions.assertThrows(InvalidReservationTimeException.class, () -> { reservationService.reserve(requestDto); });
+        Assertions.assertThrows(InvalidReservationTimeException.class, () -> { reservationService.reserveNamedLock(requestDto); });
     }
 
     @Test
     @DisplayName("예약 시간이 중복되는 경우 InvalidReservationTimeException 예외를 발생시킨다.")
     void duplicatedReservationTime() {
-        // given
-        ReservationRequestDto requestDto = new ReservationRequestDto(2L, 2L, LocalDateTime.of(2024, 8, 15, 11, 0), LocalDateTime.of(2024, 8, 15, 12, 0), 3L);
+        Reservation reservation = new Reservation();
 
-        reservationService = new ReservationService(reservationMapper, memberMapper, storeMapper);
+        reservation.setMemberSeq(1L);
+        reservation.setRestaurantId(1L);
+        reservation.setStatus("SUCCESS");
+        reservation.setMemberCount(3L);
+        reservation.setCreatedAt(LocalDateTime.now());
+        reservation.setReservationStartAt(startTime);
+        reservation.setReservationEndAt(endTime);
+        reservationMapper.saveReservation(reservation);
+
+        // given
+        ReservationRequestDto requestDto = new ReservationRequestDto(1L, 1L, startTime, endTime, 3L);
 
         // when, then
-        Assertions.assertThrows(DuplicatedReservationException.class, () -> { reservationService.reserve(requestDto); });
+        Assertions.assertThrows(DuplicatedReservationException.class, () -> { reservationService.reserveNamedLock(requestDto); });
     }
 
     @Test
     @DisplayName("예약하는 시간이 일반 또는 특수 영업 스케줄에 존재하지 않을 경우 ScheduleNotFoundException 예외를 발생시킨다.")
     void reserveNotExistSchedule() {
         // given
-        ReservationRequestDto requestDto = new ReservationRequestDto(2L, 1L, LocalDateTime.of(2024, 8, 14, 11, 0), LocalDateTime.of(2024, 8, 14, 12, 0), 3L);
-
-        reservationService = new ReservationService(reservationMapper, memberMapper, storeMapper);
+        ReservationRequestDto requestDto = new ReservationRequestDto(3L, 1L, startTime, endTime, 3L);
 
         // when, then
-        Assertions.assertThrows(ScheduleNotFoundException.class, () -> { reservationService.reserve(requestDto); });
+        Assertions.assertThrows(ScheduleNotFoundException.class, () -> { reservationService.reserveNamedLock(requestDto); });
     }
 
     @Test
     @DisplayName("유효한 예약 정보로 예약할 경우 ReservationResponseDto를 반환한다.")
     void reserveSuccess() {
         // given
-        ReservationRequestDto requestDto = new ReservationRequestDto(1L, 1L, LocalDateTime.of(2024, 8, 18, 11, 0), LocalDateTime.of(2024, 8, 18, 12, 0), 3L);
-
-        reservationService = new ReservationService(reservationMapper, memberMapper, storeMapper);
+        ReservationRequestDto requestDto = new ReservationRequestDto(1L, 1L, startTime, endTime, 3L);
 
         // when
-        ReservationResponseDto responseDto = reservationService.reserve(requestDto);
+        ReservationResponseDto responseDto = reservationService.reserveNamedLock(requestDto);
 
         // then
         Assertions.assertAll(
                 () -> Assertions.assertNotNull(responseDto),
                 () -> Assertions.assertEquals(1L, responseDto.getMemberSeq()),
                 () -> Assertions.assertEquals(1L, responseDto.getRestaurantId()),
-                () -> Assertions.assertEquals(LocalDateTime.of(2024, 8, 18, 11, 0), responseDto.getReservationStartTime()),
-                () -> Assertions.assertEquals(LocalDateTime.of(2024, 8, 18, 12, 0), responseDto.getReservationEndTime()),
+                () -> Assertions.assertEquals(startTime, responseDto.getReservationStartTime()),
+                () -> Assertions.assertEquals(endTime, responseDto.getReservationEndTime()),
                 () -> Assertions.assertEquals(3L, responseDto.getMemberCount())
         );
     }
--- src/test/resources/init.sql
+++ src/test/resources/init.sql
@@ -62,29 +62,21 @@ VALUES
 ("오소영", "syoh", "qwer123", "010-1111-2222"),
 ("메버릭", "maverick", "asdf123", "010-3333-4444");
 
-INSERT INTO reservation(member_seq, restaurant_id, status, member_count, created_at, reservation_start_at, reservation_end_at)
-VALUES
-(1, 1, 'SUCCESS', 3, NOW(), '2024-08-13 11:00:00', '2024-08-13 12:00:00'),
-(1, 2, 'SUCCESS', 5, NOW(), '2024-08-13 12:00:00', '2024-08-13 12:30:00'),
-(2, 1, 'SUCCESS', 2, NOW(), '2024-08-15 10:00:00', '2024-08-15 11:00:00'),
-(2, 2, 'SUCCESS', 1, NOW(), '2024-08-15 11:00:00', '2024-08-15 12:00:00');
-
 INSERT INTO restaurant(name, address, latitude, longitude, max_member_count, max_available_day)
 VALUES
 ('R1', '서울 강서구 마곡서로 152', 37.5676859105, 126.8259794500, 10, 5),
-('R2', '서울 강서구 마곡중앙로 136', 33.2588494316, 126.4061074950, 4, 3);
+('R2', '서울 강서구 마곡중앙로 136', 33.2588494316, 126.4061074950, 4, 3),
+('R3', '서울 마포구 양화로 151', 37.5562674192411, 126.922368229397, 20, 3);
 
 INSERT INTO daily_schedule(id, day, open_time, close_time, split_time)
 VALUES
-(1, 'MONDAY', '10:00:00', '15:00:00', 60),
-(1, 'WEDNESDAY', '10:00:00', '15:00:00', 60),
-(1, 'FRIDAY', '10:00:00', '15:00:00', 60),
-(1, 'SATURDAY', '10:00:00', '13:00:00', 30),
-(1, 'SUNDAY', '10:00:00', '13:00:00', 30),
+(1, 'MONDAY', '00:00:00', '23:59:59', 60),
+(1, 'TUESDAY', '00:00:00', '23:59:59', 60),
+(1, 'WEDNESDAY', '00:00:00', '23:59:59', 60),
+(1, 'THURSDAY', '00:00:00', '23:59:59', 60),
+(1, 'FRIDAY', '00:00:00', '23:59:59', 60),
+(1, 'SATURDAY', '00:00:00', '23:59:59', 30),
+(1, 'SUNDAY', '00:00:00', '23:59:59', 30),
 (2, 'FRIDAY', '09:00:00', '19:00:00', 60),
 (2, 'SATURDAY', '10:00:00', '19:00:00', 60),
 (2, 'SUNDAY', '10:00:00', '20:00:00', 60);
-
-INSERT INTO specific_schedule(id, date, open_time, close_time, split_time)
-VALUES
-(2, '2024-08-16', '15:00:00', '16:00:00', 60);
